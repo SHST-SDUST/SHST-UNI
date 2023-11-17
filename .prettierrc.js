91911b863b0c5ce8624556d20d98151d479120f8
WindrunnerMax <651525974@qq.com>
2023-11-17 22:00:47 +0800

    chore: format

--- .prettierrc.js
+++ .prettierrc.js
@@ -14,5 +14,5 @@ module.exports = {
     "insertPragma": false, // 是否在格式化的文件顶部插入Pragma标记，以表明该文件被prettier格式化过了
     "proseWrap": "preserve", // 按照文件原样折行
     "htmlWhitespaceSensitivity": "ignore", // html文件的空格敏感度，控制空格是否影响布局
-    "endOfLine": "lf" // 结尾是 \n \r \n\r auto
-}
+    "endOfLine": "lf", // 结尾是 \n \r \n\r auto
+};
